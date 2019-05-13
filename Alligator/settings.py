7d4c8308b02452ac1eefd4998658c5482ca70618
Kirill Sharov <kupuk090@gmail.com>
2019-05-13 18:58:05 +0300

    change settings

--- Alligator/settings.py
+++ Alligator/settings.py
@@ -198,5 +198,4 @@ STATICFILES_DIRS = [
     'static/',
 ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
